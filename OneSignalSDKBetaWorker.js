721375c09b248ec162379571ac3dfcc64d44095b
Jason Pang <jason@onesignal.com>
2016-03-22 17:11:59 -0700

    Bypass cloudflare cache for beta service workers.

--- OneSignalSDKBetaWorker.js
+++ OneSignalSDKBetaWorker.js
@@ -1,1 +1,2 @@
-importScripts('https://onesignal.com/beta_sdks/OneSignalSDKBetaWorker.js');
+var r = Math.random();
+importScripts('https://onesignal.com/beta_sdks/OneSignalSDKBetaUpdaterWorker.js?' + r);
